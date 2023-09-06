517a343107c08cd06094b22ca4d2041304d72841
pkthunder87 <pkthunder87@gmail.com>
2023-09-05 19:19:20 -0700

    Lesson 262: Creating a Reducer: Bank Account

---
+++ src/store.jsx
@@ -0,0 +1,29 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action };
+
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      // LATER
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
